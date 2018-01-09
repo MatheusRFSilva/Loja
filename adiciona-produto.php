d4deb1932113dca244c2f3bd5565d5628ab572a5
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-01-09 18:24:32 -0200

    Aula 2 - 01 Inserindo produtos no banco de dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -2,7 +2,17 @@
 <?php
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
+$conexao = mysqli_connect('sql130.main-hosting.eu','u407834309_silva','slipknot','u407834309_dbnew');
+
+$query = "insert into produtos (nome,preco) values ('{$nome}',{$preco})";
+if (mysqli_query($conexao,$query)){ ?>
+<p class="alert-success">Produto <?=$nome; ?>, <?=$preco;?> adicionado com sucesso!</p>
+<?}else{?>
+  <p class="alert-danger">Produto Não adicionado !</p>
+<?};
+
+mysqli_close($conexao);
  ?>
- <p class="alert-success">Produto <?=$nome; ?>, <?=$preco;?> adicionado com sucesso!</p>
+
 
 <? include("rodape.php")?>
